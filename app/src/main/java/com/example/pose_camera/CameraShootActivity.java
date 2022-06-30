90fd5a5ff48007cb798b8295d798d41baf5d812d
zodiacVG <zhangzibo418@outlook.com>
2022-06-30 18:04:18 +0800

    【可运行】更改gradle版本至7.1.2，加入新的布局文件

---
+++ app/src/main/java/com/example/pose_camera/CameraShootActivity.java
@@ -0,0 +1,14 @@
+package com.example.pose_camera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraShootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_shoot);
+    }
+}
